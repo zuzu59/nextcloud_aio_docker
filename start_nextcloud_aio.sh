d61f6739410b35a4036adb2b9cc2bc0b1585076f
Christian Zufferey <christian@zufferey.com>
2025-01-07 16:29:01 +0000

    first commit

---
+++ start_nextcloud_aio.sh
@@ -0,0 +1,19 @@
+# petit script pour démarrer NextCloud Hub
+# zf250106.2330
+
+
+sudo docker run \
+--init \
+--sig-proxy=false \
+--name nextcloud-aio-mastercontainer \
+--restart always \
+--publish 8080:8080 \
+--env APACHE_PORT=11000 \
+--env APACHE_IP_BINDING=0.0.0.0 \
+--env APACHE_ADDITIONAL_NETWORK="" \
+--env SKIP_DOMAIN_VALIDATION=true \
+--volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config \
+--volume /var/run/docker.sock:/var/run/docker.sock:ro \
+nextcloud/all-in-one:latest
+
+
